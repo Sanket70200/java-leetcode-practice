46813d32bb34b0cc1ca155c10ca3c90e46ceadc7
Sanket Patil <sanketpatil9850@gmail.com>
2026-02-24 08:18:32 +0530

    implementation of palindrome problem

---
+++ Practice/src/palindrome.java
@@ -0,0 +1,29 @@
+public class palindrome {
+
+    public static boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+
+        int n = x;
+        long revNum = 0;
+
+        while (n > 0) {
+            int d = n % 10;
+            revNum = revNum * 10 + d;
+            n = n / 10;
+        }
+
+        return revNum == x;
+    }
+
+    public static void main(String[] args) {
+        int number = 121;
+
+        if (isPalindrome(number)) {
+            System.out.println(number + " is a Palindrome");
+        } else {
+            System.out.println(number + " is NOT a Palindrome");
+        }
+    }
+}
