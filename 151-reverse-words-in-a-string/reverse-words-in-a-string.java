2fe28e039afb1ebf06ba341cb1e2d0b54ef73749
sanket patil <120205577+Sanket70200@users.noreply.github.com>
2026-07-03 22:42:55 +0530

    Time: 5 ms (85.45%) | Memory: 44.7 MB (32.32%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -3,33 +3,53 @@ class Solution {
     public String reverseWords(String s) {
 
        
-        s = s.trim().replaceAll("\\s+", " ");
+    //     s = s.trim().replaceAll("\\s+", " ");
 
-        char[] arr = s.toCharArray();
-        int n = arr.length;
+    //     char[] arr = s.toCharArray();
+    //     int n = arr.length;
 
-        reverse(arr, 0, n - 1);
+    //     reverse(arr, 0, n - 1);
 
 
-        int start = 0;
-        for (int end = 0; end <= n; end++) {
-            if (end == n || arr[end] == ' ') {
-                reverse(arr, start, end - 1);
-                start = end + 1;
-            }
+    //     int start = 0;
+    //     for (int end = 0; end <= n; end++) {
+    //         if (end == n || arr[end] == ' ') {
+    //             reverse(arr, start, end - 1);
+    //             start = end + 1;
+    //         }
+    //     }
+
+    //     return new String(arr);
+    // }
+
+    // private void reverse(char[] arr, int left, int right) {
+    //     while (left < right) {
+    //         char temp = arr[left];
+    //         arr[left] = arr[right];
+    //         arr[right] = temp;
+
+    //         left++;
+    //         right--;
+    //     }
+
+
+
+    /////////////////////////////////////////////////////////////////////////////////////////
+
+    String arr[]=s.split(" ");
+    StringBuffer res=new StringBuffer();
+    for(int i=arr.length-1;i>=0;i--){
+        if(arr[i].length()==0){
+            continue;
         }
+        if(res.length()==0){
+            res.append(arr[i]);
 
-        return new String(arr);
+        }else{
+            res.append(" ");
+            res.append(arr[i]);
+        }
     }
-
-    private void reverse(char[] arr, int left, int right) {
-        while (left < right) {
-            char temp = arr[left];
-            arr[left] = arr[right];
-            arr[right] = temp;
-
-            left++;
-            right--;
-        }
+    return res.toString();
     }
 }
