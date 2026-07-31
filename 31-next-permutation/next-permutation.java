96d4bfa33e96c78ff2b482a0079ce783f8716e69
sanket patil <120205577+Sanket70200@users.noreply.github.com>
2026-07-31 18:47:29 +0530

    Time: 0 ms (100.00%) | Memory: 44.5 MB (70.07%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -1,44 +1,42 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        int n=nums.length;
-        int pivot=-1;
-        for(int i=n-2;i>=0;i--){
-            if(nums[i]<nums[i+1]){
-                pivot=i;
-                break;
-            }
-        }
-            if(pivot==-1){
+   
+int n=nums.length;
+int pivot=-1;
 
-                reverse(nums,0,n-1);
-            return ;
-            
-        }
-
-        for(int i=n-1;i>=0;i--){
-            if(nums[i]>nums[pivot]){
-                swap(nums,i,pivot);
-                break;
-            }
-        }
-     reverse(nums,pivot+1,n-1);
-
+for(int i=n-2;i>=0;i--){
+    if(nums[i]<nums[i+1]){
+        pivot=i;
+        break;
     }
-     private void swap(int []nums, int i ,int j){
-        int temp=nums[i];
-        nums[i]=nums[j];
-        nums[j]=temp;
-        
-     }
+}
 
+if(pivot==-1){
+    reverse(nums,0,n-1);
+    return;
+}
 
-     private void reverse(int nums[],int left,int right){
-        while(left<right){
-            swap(nums,left,right);
-            left++;
-            right--;
-        
-     }
-
+for(int i=n-1;i>=0;i--){
+    if(nums[i]>nums[pivot]){
+        swap(nums,i,pivot);
+        break;
     }
+}
+reverse(nums,pivot+1,n-1);
+    }
+   public void swap(int[] nums,int i,int j){
+    int temp=nums[i];
+    nums[i]=nums[j];
+    nums[j]=temp;
+   }
+
+
+   public void reverse(int [] nums,int left,int right){
+    while(left<right){
+    swap(nums,left,right);
+    left++;
+    right--;
+   }
+}
+    
 }
