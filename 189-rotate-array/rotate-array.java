ea181332663b2e8ed71ad64974a57e84be20b71a
sanket patil <120205577+Sanket70200@users.noreply.github.com>
2026-07-31 18:02:29 +0530

    Time: 0 ms (100.00%) | Memory: 61.3 MB (41.38%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -2,28 +2,28 @@ import java.util.Arrays;
 class Solution {
 
     public static void main(String []args){
-        int nums[]={1,2,3,4,5,6,7};
+        int arr[]={1,2,3,4,5,6,7};
         int k=3;
-        System.out.println(Arrays.toString(rotate(nums,k)));
+        System.out.println(Arrays.toString(rotate(arr,k)));
     }
-    public  static int [] rotate(int[] nums, int k) {
-        
-    int n=nums.length;
+
+  public static int [] rotate(int arr[] ,int  k){
+    int n=arr.length;
     k=k%n;
-    reverse(nums,0,n-1);
-    reverse(nums,0,k-1);
-    reverse(nums,k,n-1);
-    return nums;
 
-    }
+    reverse(arr,0,n-1);
+    reverse(arr,0,k-1);
+    reverse(arr,k,n-1);
+    return arr;
+  }
 
-    public static void reverse(int nums[],int start,int end ){
-       while (start < end) {
-            int temp = nums[start];
-            nums[start] = nums[end];
-            nums[end] = temp;
-            start++;
-            end--;
-        }
+  public static void reverse(int arr[],int start,int end){
+    while(start<end){
+        int temp=arr[start];
+        arr[start]=arr[end];
+        arr[end]=temp;
+        start++;
+        end--;
     }
+  }
 }
