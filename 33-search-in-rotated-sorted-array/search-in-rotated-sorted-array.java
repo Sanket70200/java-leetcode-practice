5044f4d115ca7d48ccf88d6b796dd084dbf4bb93
sanket patil <120205577+Sanket70200@users.noreply.github.com>
2026-08-02 12:43:39 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (65.95%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -2,17 +2,17 @@ class Solution {
       public int search(int[] nums, int target) {
         return search(nums, target, 0, nums.length - 1);
     }
-   
-   public int search  (int []nums,int target ,int left,int right){
-    if(left>right){
-        return -1;
-    }
+   public int search(int [] nums,int target,int left,int right){
+  if(left > right){
+    return -1;
+  }
 
     int mid=left+(right-left)/2;
 
     if(nums[mid]==target){
         return mid;
     }
+
     if(nums[mid]>=nums[left]){
         if(nums[left]<=target && target<=nums[mid]){
             return search(nums,target,left,mid-1);
@@ -27,6 +27,8 @@ class Solution {
         }
     }
 
+
    }
+
     
 }
