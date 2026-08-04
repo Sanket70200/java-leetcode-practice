8bbedd42d09d3f11d9cb7addf83927b07a9123a5
sanket patil <120205577+Sanket70200@users.noreply.github.com>
2026-08-04 21:23:02 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (43.74%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.java
+++ 54-spiral-matrix/spiral-matrix.java
@@ -1,42 +1,42 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-        int rowBegin=0;
-        int rowEnd=matrix.length-1;
-        int colBegin=0;
-        int colEnd=matrix[0].length-1;
+     int rowBegin=0;
+     int rowEnd=matrix.length-1;
+     int colBegin=0;
+     int colEnd=matrix[0].length-1;
 
-        List<Integer>result=new ArrayList<>();
+    List<Integer> result =new ArrayList<>();
+    while(rowBegin<=rowEnd && colBegin<=colEnd){
 
 
-        while(rowBegin<=rowEnd && colBegin <=colEnd){
-            for(int i=colBegin;i<=colEnd;i++){
-                result.add(matrix[rowBegin][i]);
-            }
+        for(int i=colBegin;i<=colEnd;i++){
+            result.add(matrix[rowBegin][i]);
+        }
 
-            rowBegin++;
+        rowBegin++;
 
-            for(int i=rowBegin;i<=rowEnd;i++){
-                result.add(matrix[i][colEnd]);
-            }
+        for(int i=rowBegin;i<=rowEnd;i++){
+            result.add(matrix[i][colEnd]);
+        }
 
-            colEnd--;
+        colEnd--;
+if(rowBegin<=rowEnd){
+        for(int i=colEnd;i>=colBegin;i--){
+            result.add(matrix[rowEnd][i]);
+        }
+}
+        rowEnd--;
 
-             if(rowBegin<=rowEnd){
-            for(int i=colEnd;i>=colBegin;i--){
-                result.add(matrix[rowEnd][i]);
-            }
-             }
-            rowEnd--;
 
-                if(colBegin<=colEnd){
+        if(colBegin<=colEnd){
             for(int i=rowEnd;i>=rowBegin;i--){
                 result.add(matrix[i][colBegin]);
             }
-                }
-            colBegin++;
         }
 
-        return result;
+        colBegin++;
+    }
+    return result;
     }
 
 }
