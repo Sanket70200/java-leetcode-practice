9c1014c0f5972059d1b4cc4f18a7afec48810097
sanket patil <120205577+Sanket70200@users.noreply.github.com>
2026-08-04 13:39:38 +0530

    Time: 5 ms (80.99%) | Memory: 46 MB (33.94%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -19,28 +19,26 @@ class Solution {
         //     return false;
         // }
 
+if(x<0){
+    return false;
+}
+
+int revnum=0;
+int n=x;
+while(n>0){
+    int digit=n%10;
+    revnum=revnum*10+digit;
+    n=n/10;
+}
+
+if(revnum==x){
+    return true;
+}else{
+    return false;
+}
 
 
 
-
-        if(x<0)
-        return false;
-
-
-        int n=x;
-        int revnum=0;
-        while(n>0){
-            int d=n%10;
-            revnum=revnum*10+d;
-             n=n/10;
-        
-        }
-
-        if(revnum==x){
-            return true;
-        }else{
-            return false;
-
-        }
     }
+    
 }
