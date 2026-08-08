c134d3d7ddf81292362371d7f643f6aeee7ad352
sanket patil <120205577+Sanket70200@users.noreply.github.com>
2026-08-09 00:08:24 +0530

    Time: 0 ms (100.00%) | Memory: 48.5 MB (21.83%) - LeetSync

--- 344-reverse-string/reverse-string.java
+++ 344-reverse-string/reverse-string.java
@@ -1,8 +1,21 @@
 class Solution {
     public void reverseString(char[] s) {
-        StringBuilder reverse=new StringBuilder(new String(s)).reverse();
-        for(int i=0;i<s.length;i++){
-            s[i]=reverse.charAt(i);
+        // StringBuilder reverse=new StringBuilder(new String(s)).reverse();
+        // for(int i=0;i<s.length;i++){
+        //     s[i]=reverse.charAt(i);
+        // }
+
+
+
+        int n=s.length;
+        int left=0;
+        int right=n-1;
+        while(left<right){
+            char ch=s[left];
+            s[left]=s[right];
+            s[right]=ch;
+            left++;
+            right--;
         }
     }
 }
