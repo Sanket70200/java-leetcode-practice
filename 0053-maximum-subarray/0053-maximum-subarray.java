ae3daab757340d21ee9a000b75c51fbd8cfce6c0
sanket patil <120205577+Sanket70200@users.noreply.github.com>
2026-08-14 19:36:17 +0530

    Time: 1 ms (99.95%), Space: 76.9 MB (93.08%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,14 +1,14 @@
 //Kadane's algorithm
 class Solution {
     public int maxSubArray(int[] nums) {
-    int currentSum=nums[0];
-    int maxSum=nums[0];
-    for(int i=1;i<nums.length;i++){
-        currentSum=Math.max(currentSum+nums[i],nums[i]);
+    // int currentSum=nums[0];
+    // int maxSum=nums[0];
+    // for(int i=1;i<nums.length;i++){
+    //     currentSum=Math.max(currentSum+nums[i],nums[i]);
 
-        maxSum=Math.max(currentSum,maxSum);
-    }
-    return maxSum ;
+    //     maxSum=Math.max(currentSum,maxSum);
+    // }
+    // return maxSum ;
 
             // int maxSum=Integer.MIN_VALUE;
             // for(int i=0;i<nums.length;i++ ){
@@ -22,5 +22,17 @@ class Solution {
             // }
 
             // return maxSum;
+
+
+
+            int maxSum=nums[0];
+            int currentSum=nums[0];
+
+            for(int i=1;i<nums.length;i++){
+
+                currentSum=Math.max(currentSum+nums[i],nums[i]);
+                maxSum=Math.max(currentSum,maxSum);
+            }
+            return maxSum;
     }
 }
