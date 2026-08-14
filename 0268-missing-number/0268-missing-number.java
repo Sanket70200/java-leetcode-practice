68f36eeddcf7d166972c5a87f03337a782354664
sanket patil <120205577+Sanket70200@users.noreply.github.com>
2026-08-14 11:42:55 +0530

    Time: 0 ms (100%), Space: 47.3 MB (49.62%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+      int n=nums.length;
+      int actualvalue=n*(n+1)/2;
+      int currentSum=0;
+      for(int i=0;i<nums.length;i++){
+currentSum+=nums[i];
+      }
+
+      int ans=actualvalue-currentSum;
+      return ans;
+    }
+}
