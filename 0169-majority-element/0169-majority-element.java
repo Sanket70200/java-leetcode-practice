bd8377f76fc9433834f26329c8b9fd7ac63cc54d
sanket patil <120205577+Sanket70200@users.noreply.github.com>
2026-08-14 12:54:32 +0530

    Time: 9 ms (39.57%), Space: 55.8 MB (18.29%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,60 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+
+// int n=nums.length;
+// int freq=0;
+// int ans=0;
+// for(int i=0;i<n;i++){
+//     if(freq==0){
+//         ans= nums[i];
+//     }
+//     if(ans == nums[i]){
+//         freq++;
+//     }else{
+//         freq--;
+//     }
+
+// }
+// return ans;
+
+
+
+//broute force
+// for(int i=0;i<nums.length;i++){
+//     int count=0;
+//     for(int j=0;j<nums.length;j++){
+//         if(nums[i]==nums[j]){
+//             count++;
+//         }
+//     }
+
+
+// if(count>nums.length/2){
+//     return nums[i];
+// }
+//     }
+// return -1;
+
+
+
+
+//sorting
+
+int n=nums.length;
+int freq=1;
+Arrays.sort(nums);
+for(int i=1;i<n;i++){
+    if(nums[i]==nums[i-1]){
+        freq++;
+    }else{
+        freq=1;
+    }
+
+    if(freq>n/2){
+        return nums[i];
+    }
+}
+return nums[0];
+}
+
+}
