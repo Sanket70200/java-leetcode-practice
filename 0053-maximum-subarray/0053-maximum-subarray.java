be164a20cff8886849b8336ded9b80efd594201f
sanket patil <120205577+Sanket70200@users.noreply.github.com>
2026-08-15 11:47:52 +0530

    Time: 1 ms (99.95%), Space: 77.1 MB (66.91%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -24,15 +24,14 @@ class Solution {
             // return maxSum;
 
 
+int maxSum=nums[0];
+int currentSum=nums[0];
+for(int i=1;i<nums.length;i++){
+    currentSum=Math.max(currentSum+nums[i],nums[i]);
+    maxSum=Math.max(currentSum,maxSum);
+}
+return maxSum;
 
-            int maxSum=nums[0];
-            int currentSum=nums[0];
-
-            for(int i=1;i<nums.length;i++){
-
-                currentSum=Math.max(currentSum+nums[i],nums[i]);
-                maxSum=Math.max(currentSum,maxSum);
-            }
-            return maxSum;
+            
     }
 }
