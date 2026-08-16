bae8ca7d9dea1627a928e5f91d6e0a850ea80eeb
sanket patil <120205577+Sanket70200@users.noreply.github.com>
2026-08-16 23:01:44 +0530

    Time: 2 ms (71.97%), Space: 47.1 MB (88.73%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -2,26 +2,42 @@
 class Solution {
     public int maxProduct(int[] nums) {
         
-        int maxp=Integer.MIN_VALUE;
-        int curr=1;
-        for(int i=0;i<nums.length;i++){
-            curr=curr * nums[i];
-            maxp=Math.max(curr,maxp);
-            if(curr == 0){
-                curr=1;
+    //     int maxp=Integer.MIN_VALUE;
+    //     int curr=1;
+    //     for(int i=0;i<nums.length;i++){
+    //         curr=curr * nums[i];
+    //         maxp=Math.max(curr,maxp);
+    //         if(curr == 0){
+    //             curr=1;
 
 
 
-            }
-        }
-           curr=1;
-        for(int i=nums.length-1;i>=0;i--){
-            curr=curr * nums[i];
-            maxp=Math.max(curr,maxp);
-            if(curr==0){
-                curr=1;
-            }
-        }
-        return maxp;
+    //         }
+    //     }
+    //        curr=1;
+    //     for(int i=nums.length-1;i>=0;i--){
+    //         curr=curr * nums[i];
+    //         maxp=Math.max(curr,maxp);
+    //         if(curr==0){
+    //             curr=1;
+    //         }
+    //     }
+    //     return maxp;
+
+
+    int n=nums.length;
+    int leftproduct=1;
+    int rightproduct=1;
+    int ans=nums[0];
+    for(int i=0;i<n;i++){
+        leftproduct=leftproduct==0?1:leftproduct;
+        rightproduct=rightproduct==0?1:rightproduct;
+
+        leftproduct*=nums[i];
+        rightproduct*= nums[n-1-i];
+
+        ans=Math.max(ans,Math.max(leftproduct,rightproduct));
     }
+    return ans;
+     }
 }
