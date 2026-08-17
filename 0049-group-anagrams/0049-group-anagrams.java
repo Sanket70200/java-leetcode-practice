b6c2572798edd04e0dce90910d55d74b5174f2c4
sanket patil <120205577+Sanket70200@users.noreply.github.com>
2026-08-17 11:35:17 +0530

    Time: 7 ms (72.35%), Space: 49.8 MB (32.45%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -18,14 +18,27 @@ class Solution {
 // return new ArrayList<>(map.values());
 
 
-Map<String,List<String>>map=new HashMap<>();
+// Map<String,List<String>>map=new HashMap<>();
+// for(String word:strs){
+//     char[] charSet=word.toCharArray();
+//     Arrays.sort(charSet);
+//     String key=new String(charSet);
+
+//     map.computeIfAbsent(key,k-> new ArrayList<>()).add(word);
+// }
+// return new ArrayList<>(map.values());
+
+
+Map<String ,List<String>>map=new HashMap<>();
+
 for(String word:strs){
-    char[] charSet=word.toCharArray();
-    Arrays.sort(charSet);
-    String key=new String(charSet);
+    char[] chars=word.toCharArray();
+    Arrays.sort(chars);
+    String key = new String(chars);
 
     map.computeIfAbsent(key,k-> new ArrayList<>()).add(word);
 }
+
 return new ArrayList<>(map.values());
 }
 }
