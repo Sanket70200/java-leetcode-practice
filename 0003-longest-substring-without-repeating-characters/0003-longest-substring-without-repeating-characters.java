bb63a5ea78897712c391ff420b0c6f4a0acd9f07
sanket patil <120205577+Sanket70200@users.noreply.github.com>
2026-08-17 11:42:01 +0530

    Time: 65 ms (22.22%), Space: 48.1 MB (8.69%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -16,20 +16,39 @@ class Solution {
 
     //  return maxLength;
 
-    int left=0;
-    int maxLength=0;
-    Set<Character>charSet=new HashSet<>();
-    for(int right=0;right<s.length();right++){
+//     int left=0;
+//     int maxLength=0;
+//     Set<Character>charSet=new HashSet<>();
+//     for(int right=0;right<s.length();right++){
 
-while(charSet.contains(s.charAt(right))){
-    charSet.remove(s.charAt(left));
+// while(charSet.contains(s.charAt(right))){
+//     charSet.remove(s.charAt(left));
+//     left++;
+// }
+//         charSet.add(s.charAt(right));
+//         maxLength=Math.max(maxLength,right-left+1);
+//     }
+//       return maxLength;
+
+
+
+int left=0;
+int maxLength=0;
+
+Set<Character>set=new HashSet<>();
+for(int right=0;right<s.length();right++){
+
+while(set.contains(s.charAt(right))){
+    set.remove(s.charAt(left));
     left++;
 }
-        charSet.add(s.charAt(right));
-        maxLength=Math.max(maxLength,right-left+1);
+    set.add(s.charAt(right));
+    maxLength=Math.max(maxLength,right-left+1);
+}
+
+ return maxLength;
     }
-      return maxLength;
-    }
+   
   
     
 }
