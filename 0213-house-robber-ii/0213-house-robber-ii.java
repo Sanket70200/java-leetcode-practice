7a1d1dba2eecc4767bceec78a2897b6ac1493ce1
sanket patil <120205577+Sanket70200@users.noreply.github.com>
2026-08-24 23:22:07 +0530

    Time: 0 ms (100%), Space: 42.7 MB (78.2%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -42,36 +42,36 @@ public int rob(int []nums){
     if(nums.length<2){
         return nums[0];
     }
+int skipFirst[]=new int[nums.length];
+int skipLast[]=new int [nums.length];
 
+for(int i=0;i<nums.length-1;i++){
+    skipFirst[i]=nums[i+1];
+    skipLast[i]=nums[i];
+}
+    int lootskipFirst=robHelper(skipFirst);
+    int lootskipLast=robHelper(skipLast);
 
-    int skipfirstHome[]=new int[nums.length-1];
-    int skiplastHome[]=new int[nums.length-1];
-
-    for(int i=0;i<nums.length-1;i++){
-        skipfirstHome[i]=nums[i+1];
-        skiplastHome[i]=nums[i];
-    }
-        int lootSkipFirst=robHelper(skipfirstHome);
-        int lootSkipLast=robHelper(skiplastHome);
-return Math.max(lootSkipFirst,lootSkipLast);
-
-    
+    return Math.max(lootskipFirst,lootskipLast);
 }
 
+    
+
+
 public int robHelper(int nums[]){
-    if(nums.length<2){
-        return nums[0];
-    }
+  if(nums.length<2){
+    return nums[0];
+  }
 
-    int dp[]=new int [nums.length];
-    dp[0]=nums[0];
-    dp[1]=Math.max(nums[0],nums[1]);
+  int dp[]=new int [nums.length];
+  dp[0]=nums[0];
+  dp[1]=Math.max(nums[0],nums[1]);
 
-    for(int i=2;i<nums.length;i++){
-        dp[i]=Math.max(dp[i-2]+nums[i],dp[i-1]);
-    }
+  for(int i=2;i<nums.length;i++){
+    dp[i]=Math.max(dp[i-2]+nums[i],dp[i-1]);
+  }
 
-     return dp[nums.length-1];
+  return dp[nums.length-1];
 }
 
 }
