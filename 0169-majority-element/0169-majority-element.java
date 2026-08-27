477ac7e88b6a8034eb3c5cf325c4fb5347411a07
sanket patil <120205577+Sanket70200@users.noreply.github.com>
2026-08-27 09:48:54 +0530

    Time: 1912 ms (5.1%), Space: 55.8 MB (17.95%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -40,21 +40,39 @@ class Solution {
 
 //sorting
 
+// int n=nums.length;
+// int freq=1;
+// Arrays.sort(nums);
+// for(int i=1;i<n;i++){
+//     if(nums[i]==nums[i-1]){
+//         freq++;
+//     }else{
+//         freq=1;
+//     }
+
+//     if(freq>n/2){
+//         return nums[i];
+//     }
+// }
+// return nums[0];
+
+
+
 int n=nums.length;
-int freq=1;
-Arrays.sort(nums);
-for(int i=1;i<n;i++){
-    if(nums[i]==nums[i-1]){
-        freq++;
-    }else{
-        freq=1;
+for(int i=0;i<n;i++){
+    int count=0;
+    for(int j=0;j<n;j++){
+        if(nums[i]==nums[j]){
+            count++;
+        }
     }
 
-    if(freq>n/2){
+    if(count>n/2){
         return nums[i];
     }
 }
-return nums[0];
+
+return -1;
 }
 
 }
