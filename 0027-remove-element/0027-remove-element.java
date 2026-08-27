077f840f18a2d3467d42ed493a307f0539b5f308
sanket patil <120205577+Sanket70200@users.noreply.github.com>
2026-08-27 09:44:47 +0530

    Time: 0 ms (100%), Space: 43.5 MB (61.68%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -1,12 +1,22 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-       int j=0;
-       for(int i=0;i<nums.length;i++){
-        if(nums[i]!=val){
-            nums[j]=nums[i];
-            j++;
-        }
-       }
-       return j;
+    //    int j=0;
+    //    for(int i=0;i<nums.length;i++){
+    //     if(nums[i]!=val){
+    //         nums[j]=nums[i];
+    //         j++;
+    //     }
+    //    }
+    //    return j;
+
+
+    int ptr=0;
+    for(int i=0;i<nums.length;i++){
+    if(nums[i]!=val){
+        nums[ptr]=nums[i];
+        ptr++;
+    }
+    }
+    return ptr;
     }
 }
