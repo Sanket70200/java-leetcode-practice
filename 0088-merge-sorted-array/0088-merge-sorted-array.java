0e203a999be2167b10967ebbb342f840b7e79e50
sanket patil <120205577+Sanket70200@users.noreply.github.com>
2026-08-29 23:02:43 +0530

    Time: 0 ms (100%), Space: 43.9 MB (30.42%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,19 +1,40 @@
 class Solution {
     public void merge(int[] A, int m, int[] B, int n) {
+    // int idx=m+n-1;
+    // int i=m-1;
+    // int j=n-1;
+
+    // while(i>=0 && j>=0){
+    //     if(A[i]>=B[j]){
+    //         A[idx--]=A[i--];
+    //     }else{
+    //         A[idx--]=B[j--];
+    //     }
+    // }
+
+    // while(j>=0){
+    //     A[idx--]=B[j--];
+    // }
+
+
+
+
     int idx=m+n-1;
     int i=m-1;
     int j=n-1;
 
-    while(i>=0 && j>=0){
+    while(j>=0 && i>=0){
         if(A[i]>=B[j]){
-            A[idx--]=A[i--];
-        }else{
-            A[idx--]=B[j--];
-        }
+            A[idx--] =A[i--];       
+            }else{
+                A[idx--]=B[j--];
+            }
     }
 
     while(j>=0){
         A[idx--]=B[j--];
     }
+
+
     }
 }
