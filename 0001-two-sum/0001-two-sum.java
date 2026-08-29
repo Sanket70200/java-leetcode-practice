92309a189e8bf72080326f2e01bb6d15c90872c2
sanket patil <120205577+Sanket70200@users.noreply.github.com>
2026-08-29 23:22:17 +0530

    Time: 50 ms (5.86%), Space: 47.1 MB (40.97%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -27,15 +27,27 @@ class Solution {
 
 
 
-    Map<Integer,Integer> map=new HashMap<>();
-    for(int i=0;i<nums.length;i++){
-        int complament=target-nums[i];
+    // Map<Integer,Integer> map=new HashMap<>();
+    // for(int i=0;i<nums.length;i++){
+    //     int complament=target-nums[i];
 
-        if(map.containsKey(complament)){
-            return new int[]{map.get(complament),i};
+    //     if(map.containsKey(complament)){
+    //         return new int[]{map.get(complament),i};
+    //     }
+    //     map.put(nums[i],i);
+    // }
+    // return new int []{};
+
+
+    int ans[]=new int [2];
+    for(int i=0;i<nums.length;i++){
+        for(int j=i+1;j<nums.length;j++){
+            if(nums[i]+nums[j]==target){
+                ans[0]=i;
+                ans[1]=j;
+            }
         }
-        map.put(nums[i],i);
     }
-    return new int []{};
+    return ans;
 }
 }
