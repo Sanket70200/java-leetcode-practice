d8a6fd4fc1e4e9fed9ac18c8779566f8dc1384b5
sanket patil <120205577+Sanket70200@users.noreply.github.com>
2026-08-30 15:52:11 +0530

    Time: 43 ms (8.63%), Space: 96 MB (5.97%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -26,29 +26,52 @@ class Solution {
         //  }
         //  return max;
 
-        HashMap<Integer,Boolean> hm=new HashMap<>();
+        // HashMap<Integer,Boolean> hm=new HashMap<>();
+        // for(int i=0;i<nums.length;i++){
+        //     hm.put(nums[i],false);
+        // }
+
+        // for(int key:hm.keySet()){
+        //     if(hm.containsKey(key-1)==false){
+        //         hm.put(key,true);
+        //     }
+        // }
+
+        // int max=0;
+        // for(int key:hm.keySet()){
+        //     int k=1;
+        //      if(hm.get(key)==true){
+        //     while(hm.containsKey(key+k)==true){
+        //         k++;
+        //     }
+        // }
+
+        // max=Math.max(max,k);
+        // }
+        // return max;
+
+
+        HashMap<Integer,Boolean>hm=new HashMap<>();
         for(int i=0;i<nums.length;i++){
             hm.put(nums[i],false);
-        }
+             }
 
-        for(int key:hm.keySet()){
-            if(hm.containsKey(key-1)==false){
-                hm.put(key,true);
-            }
-        }
+             for(int key:hm.keySet()){
+                if(hm.containsKey(key-1)==false){
+                    hm.put(key,true);
+                }
+             }
 
-        int max=0;
-        for(int key:hm.keySet()){
-            int k=1;
-             if(hm.get(key)==true){
-            while(hm.containsKey(key+k)==true){
-                k++;
-            }
-        }
-
-        max=Math.max(max,k);
-        }
-        return max;
-  
+             int max=0;
+             for(int key:hm.keySet()){
+                int k=1;
+                if(hm.get(key)==true){
+                    while(hm.containsKey(key+k)==true){
+                        k++;
+                    }
+                }
+               max=  Math.max(max,k);
+             }
+ return max;
     }
 }
