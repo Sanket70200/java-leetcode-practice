04b6a28a052d76c7455db69f7b53bad077ad6901
sanket patil <120205577+Sanket70200@users.noreply.github.com>
2026-08-30 14:56:40 +0530

    Time: 62 ms (35.19%), Space: 48.2 MB (6.96%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -32,21 +32,22 @@ class Solution {
 
 
 
-int left=0;
-int maxLength=0;
 
+
+int left=0;
+int maxlength=0;
 Set<Character>set=new HashSet<>();
 for(int right=0;right<s.length();right++){
 
-while(set.contains(s.charAt(right))){
-    set.remove(s.charAt(left));
-    left++;
-}
-    set.add(s.charAt(right));
-    maxLength=Math.max(maxLength,right-left+1);
-}
 
- return maxLength;
+    while(set.contains(s.charAt(right))){
+        set.remove(s.charAt(left));
+        left++;
+    }
+    set.add(s.charAt(right));
+    maxlength=Math.max(maxlength,right-left+1);
+}
+return maxlength;
     }
    
   
