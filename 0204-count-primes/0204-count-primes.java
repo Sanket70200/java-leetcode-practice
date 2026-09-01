0de98d689a1998197176b598b42a79b809a4483b
sanket patil <120205577+Sanket70200@users.noreply.github.com>
2026-09-01 11:26:45 +0530

    Time: 677 ms (18.08%), Space: 79.3 MB (23.69%) - LeetHub

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -1,28 +1,8 @@
 class Solution {
     public int countPrimes(int n) {
-        // boolean [] composite=new boolean [n];
-        // int limit=(int)Math.sqrt(n);
-        // for(int i=2;i<=limit;i++){
-        //     if(composite[i]==false){
-        //         for(int j=i*i;j<n;j+=i){
-        //             composite[j]=true;
-        //         }
-        //     }
-        // }
-
-        // int count=0;
-        // for( int i=2;i<n;i++){
-        //     if(composite[i]==false){
-        //         count++;
-        //     }
-        // }
-        // return count;
-
-
-        boolean [] composite =new boolean [n];
-        int limit=(int )Math.sqrt(n);
+        boolean [] composite=new boolean [n];
+        int limit=(int)Math.sqrt(n);
         for(int i=2;i<=limit;i++){
-
             if(composite[i]==false){
                 for(int j=i*i;j<n;j+=i){
                     composite[j]=true;
@@ -31,11 +11,28 @@ class Solution {
         }
 
         int count=0;
-        for(int i=2;i<n;i++){
-        if(composite[i]==false){
-            count++;
+        for( int i=2;i<n;i++){
+            if(composite[i]==false){
+                count++;
+            }
         }
-    }
-    return count;
+        return count;
+
+
+        // int count=0;
+        // for(int i=2;i<n;i++){
+        //     boolean isnumber=true;
+        //     for(int j=2;j<i;j++){
+        //         if(i%j==0){
+        //             isnumber=false;
+        //             break;
+        //         }
+        //     }
+
+        //     if(isnumber){
+        //         count++;
+        //     }
+        // }
+        // return count;
 }
 }
