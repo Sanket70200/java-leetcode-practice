962cc09b6ba4f93ce8c14f3b5b670b13870e71df
sanket patil <120205577+Sanket70200@users.noreply.github.com>
2026-09-05 22:01:55 +0530

    Time: 9 ms (39.36%), Space: 55.6 MB (46.4%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,60 +1,57 @@
 class Solution {
     public int majorityElement(int[] nums) {
 
-// int n=nums.length;
-// int freq=0;
-// int ans=0;
-// for(int i=0;i<n;i++){
-//     if(freq==0){
-//         ans= nums[i];
-//     }
-//     if(ans == nums[i]){
-//         freq++;
-//     }else{
-//         freq--;
-//     }
+        // int n=nums.length;
+        // int freq=0;
+        // int ans=0;
+        // for(int i=0;i<n;i++){
+        //     if(freq==0){
+        //         ans= nums[i];
+        //     }
+        //     if(ans == nums[i]){
+        //         freq++;
+        //     }else{
+        //         freq--;
+        //     }
 
-// }
-// return ans;
+        // }
+        // return ans;
 
+        //broute force
+        // for(int i=0;i<nums.length;i++){
+        //     int count=0;
+        //     for(int j=0;j<nums.length;j++){
+        //         if(nums[i]==nums[j]){
+        //             count++;
+        //         }
+        //     }
 
+        // if(count>nums.length/2){
+        //     return nums[i];
+        // }
+        //     }
+        // return -1;
 
-//broute force
-// for(int i=0;i<nums.length;i++){
-//     int count=0;
-//     for(int j=0;j<nums.length;j++){
-//         if(nums[i]==nums[j]){
-//             count++;
-//         }
-//     }
-
-
-// if(count>nums.length/2){
-//     return nums[i];
-// }
-//     }
-// return -1;
-
+        //sorting
 
 
-
-//sorting
-
-Arrays.sort(nums);
-int freq=1;
-for(int i=1;i<nums.length;i++){
-    if(nums[i]==nums[i-1]){
-         freq++;
-    }else{
-        freq=1;
+       Arrays.sort(nums);
+       int freq=1;
+       for(int i=1;i<nums.length;i++){
+        if(nums[i]==nums[i-1]){
+            freq++;
+        }else{
+            freq=1;
+        }
+int count=0;
+    if(freq>nums.length/2){
+       return  nums[i];
     }
 
+       }
 
-if(freq>nums.length/2){
-    return nums[i];
-}
-}
-return nums[0];
-}
+       return nums[0];
+
+    }
 
 }
